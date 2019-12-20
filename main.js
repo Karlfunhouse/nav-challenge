387de45dee50a3eba2a5e056094020179e755ccd
Karl Nielsen <karl.g.nielsen@gmail.com>
2019-12-20 10:31:28 -0700

    :crystal_ball: Added functions to show when a button is selected and also deselect the other buttons so only one button is selected at once; It's wet AF but it's all my brain can fathom at the moment;  Will refactor if there's time

--- main.js
+++ main.js
@@ -4,12 +4,55 @@ var wishlistButton = document.querySelector('.wishlist-js');
 var archivedButton = document.querySelector('.archived-js')
 var navButton = document.querySelectorAll('.nav-option');
 
-coursesButton.addEventListener('click', selectButton);
-collectionsButton.addEventListener('click', selectButton);
-wishlistButton.addEventListener('click', selectButton);
-archivedButton.addEventListener('click', selectButton);
+coursesButton.addEventListener('click', selectCourses);
+collectionsButton.addEventListener('click', selectCollections);
+wishlistButton.addEventListener('click', selectWishlist);
+archivedButton.addEventListener('click', selectArchived);
 
-function selectButton() {
-  console.log("fun")
-  event.target.classList.add('active')
+// function selectButton() {
+//   event.target.classList.add('active');
+
+
+function selectCourses() {
+  if (event.target.classList.contains('active')) {
+    console.log('active')
+  } else {
+    coursesButton.classList.add('active');
+    collectionsButton.classList.remove('active');
+    wishlistButton.classList.remove('active');
+    archivedButton.classList.remove('active');
+  }
+}
+
+function selectCollections() {
+  if (event.target.classList.contains('active')) {
+    console.log('active')
+  } else {
+    collectionsButton.classList.add('active');
+    coursesButton.classList.remove('active');
+    wishlistButton.classList.remove('active');
+    archivedButton.classList.remove('active');
+  }
+}
+
+function selectWishlist() {
+  if (event.target.classList.contains('active')) {
+    console.log('active')
+  } else {
+    wishlistButton.classList.add('active');
+    collectionsButton.classList.remove('active');
+    coursesButton.classList.remove('active');
+    archivedButton.classList.remove('active');
+  }
+}
+
+function selectArchived() {
+  if (event.target.classList.contains('active')) {
+    console.log('alive')
+  } else {
+    archivedButton.classList.add('active');
+    wishlistButton.classList.remove('active');
+    collectionsButton.classList.remove('active');
+    coursesButton.classList.remove('active');
+  }
 }
